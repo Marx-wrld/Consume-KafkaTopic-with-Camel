5f493af32e0948bd48cf4842b2a0c0dde723ade7
Marx-wrld <iongonga@kabarak.ac.ke>
2024-02-24 00:47:45 +0300

    Bug fixes and API config

--- src/main/java/com/ck/CamelKafka/Consumer/KafkaConsumer.java
+++ src/main/java/com/ck/CamelKafka/Consumer/KafkaConsumer.java
@@ -1,11 +1,17 @@
 package com.ck.CamelKafka.Consumer;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class KafkaConsumer extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("kafka:kafka-topic1?brokers=localhost:9092")
-                .log("Message received is : ${body}");
+                .log("Message received is : ${body}")
+                .to("direct:processMessage"); // Route the message to processMessage endpoint
+
+        from("direct:processMessage")
+                .log("Processing message: ${body}"); // Process the message received from Kafka
     }
 }
